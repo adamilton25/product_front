74e27069ce8929f61ecc2f18a458a502c6b8b1fd
adamilton25 <adamilton250899@gmail.com>
2022-08-29 21:09:55 -0300

    default

---
+++ src/views/AboutView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <table class="table">
+  <thead class="table-dark">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrição</th>
+      <th scope="col">Valor</th>
+      <th scope="col">Quantidade</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr :v-for="(item in items)" v-show="items != []">
+      <th scope="row">{{item}}</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+  </table>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name:"AboutView",
+    data(){
+      return {
+        items:[]
+      }
+    },
+    methods: {
+      showAllProducts(){
+        axios 
+        .get('http://127.0.0.1:3000/v1/products')
+        .then((response) => {
+          this.items = response.data['products'];
+        }) 
+      }
+    },
+    mounted(){
+      this.showAllProducts();
+    }
+  }
+</script>
